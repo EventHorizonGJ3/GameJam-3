c3acf340831cf7ae9fba80a8777c97b93df7f3d4
alessio picoco <alessio.picoco@gmail.com>
2024-06-09 20:12:21 +0200

    c-c-c COMBO SYSTEM

--- Assets/Scripts/Combact/PlayerComboM.cs
+++ Assets/Scripts/Combact/PlayerComboM.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,19 +13,31 @@ public class PlayerComboM : MonoBehaviour
 	[SerializeField] WeaponsSO punches;
 	[SerializeField] string attackAnimationName;
 
+	[Tooltip("the time it waits after the animation ended")]
 	public float AttackDelay = 0.2f;
-	public float ComboResetTime = 2f;
 
-	float lastAttackTime;
-	float lastCombotime;
+	[Tooltip("the time it waits for the next input before resetting the combo")]
+	public float ComboResetTime = 1f;
+	float lastAttackTime = 0;
+	float currentAttackDelay, currentComboResetTime;
 	int comboCounter;
-	IEnumerator resetCombo;
+	Coroutine resetCombo;
 	Animator anim;
+	ActionMap inputs;
 
+	private void Awake()
+	{
+		inputs = InputManager.ActionMap;
+	}
 	private void OnEnable()
 	{
-		InputManager.ActionMap.Player.Attack.performed += StartAttack;
+		inputs.Player.Attack.started += StartAttack;
 	}
+	private void OnDisable()
+	{
+		inputs.Player.Attack.started -= StartAttack;
+	}
+
 	private void Start()
 	{
 		UpdateCurrentWeapon(punches);
@@ -39,31 +52,41 @@ public class PlayerComboM : MonoBehaviour
 	void UpdateCurrentWeapon(WeaponsSO _NewWeapon)
 	{
 		currentWeapon = _NewWeapon;
+		Debug.Log("currentWeapon = " + currentWeapon.name);
 	}
 
 	void StartAttack(InputAction.CallbackContext _Context)
 	{
-		if (resetCombo != null)
-			StopCoroutine(resetCombo);
-
-		if (Time.time - lastAttackTime > AttackDelay)
+		if (comboCounter <= currentWeapon.AttackCombo.Count)
 		{
-			lastAttackTime = Time.time;
-
-			anim.runtimeAnimatorController = currentWeapon.AttackCombo[comboCounter].AnimOverrider;
-			anim.Play(attackAnimationName);
-
-
-
-			if (comboCounter + 1 >= currentWeapon.AttackCombo.Count)
+			//Debug.Log("is resetCombo null? " + resetCombo.IsUnityNull());
+			if (resetCombo != null)
 			{
-				currentWeapon.LastAttack(currentWeapon.AttackCombo[comboCounter].Dmg);
-				comboCounter = 0;
+				StopAllCoroutines();
+				resetCombo = null;
 			}
-			else
+
+			if (Time.time - lastAttackTime > currentAttackDelay)
 			{
+				lastAttackTime = Time.time;
+
+				// animation
+				anim.runtimeAnimatorController = currentWeapon.AttackCombo[comboCounter].AnimOverrider;
+				anim.Play(attackAnimationName);
+
+				currentAttackDelay = anim.GetCurrentAnimatorStateInfo(0).length + AttackDelay;
+				currentComboResetTime = anim.GetCurrentAnimatorStateInfo(0).length + ComboResetTime;
+
 				currentWeapon.OnAttack?.Invoke(currentWeapon.AttackCombo[comboCounter].Dmg);
+
 				comboCounter++;
+
+				if (comboCounter >= currentWeapon.AttackCombo.Count)
+				{
+					currentWeapon.LastAttack?.Invoke(currentWeapon.AttackCombo[comboCounter].Dmg);
+					comboCounter = 0;
+				}
+
 			}
 		}
 	}
@@ -72,14 +95,15 @@ public class PlayerComboM : MonoBehaviour
 	{
 		if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f && anim.GetCurrentAnimatorStateInfo(0).IsName(attackAnimationName))
 		{
-			resetCombo = EndCombo();
-			StartCoroutine(resetCombo);
+			//  if null then:
+			resetCombo ??= StartCoroutine(EndCombo());
 		}
 	}
 
 	IEnumerator EndCombo()
 	{
-		yield return new WaitForSeconds(ComboResetTime);
+		yield return new WaitForSeconds(currentComboResetTime);
+		Debug.Log("we have waited");
 		comboCounter = 0;
 	}
 }
