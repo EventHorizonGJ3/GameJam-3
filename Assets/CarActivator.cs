7b922dcb1449c709641eafbd3339bf2f69683cbd
emacrapa <emanuele.crapanzano02@gmail.com>
2024-06-20 01:37:45 +0200

    tutte cose (Parking Level almost completed)

---
+++ Assets/CarActivator.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CarActivator : MonoBehaviour
+{
+    [SerializeField, Range(0, 1f)] private float ChanceToSpawn;
+    private float rand;
+    [SerializeField] private GameObject[] Cars;
+
+    private void Awake()
+    {
+        foreach (GameObject Car in Cars)
+        {
+            rand = Random.Range(0, 1f);
+            if (rand < ChanceToSpawn)
+            {
+                Car.SetActive(true);
+            }
+        }
+    }
+}
