65a4372c30d6e5a93091930052b55dd81c4ff5ef
emacrapa <emanuele.crapanzano02@gmail.com>
2024-06-25 05:57:03 +0200

    madness level 78%

--- Assets/Scripts/Objects/BreakableObject.cs
+++ Assets/Scripts/Objects/BreakableObject.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System.Collections;
 public class BreakableObject : MonoBehaviour, IBreakable
 {
 	[field: SerializeField] public float HP { get; set; }
@@ -7,19 +7,20 @@ public class BreakableObject : MonoBehaviour, IBreakable
 	[SerializeField] public GameObject OriginalObj; //ByEma
 	[SerializeField] private GameObject spawnPoint;
 	[SerializeField] bool dealOneDmg;
-
+	[SerializeField] private bool deactivateColliderOnBreak; //ByEma
+	[SerializeField] private float respawnTime = 10f; //ByEma
 	public bool IsBroken { get; set; }
 	public Transform colliderTransform { get; set; }
 	
-	/*
-	MeshRenderer mesh;
+	private Collider objectCollider;
+	
+	//MeshRenderer mesh;
 
     private void Awake()
     {
-        mesh = GetComponent<MeshRenderer>();
+        //mesh = GetComponent<MeshRenderer>(); //ByEma
+        objectCollider = GetComponent<Collider>(); //ByEma
     }
-    */
-    //CommentedByEma
 
 
     public void Break()
@@ -30,7 +31,13 @@ public class BreakableObject : MonoBehaviour, IBreakable
 		OriginalObj.SetActive(false); //ByEma
 		//mesh.enabled = false; //CommentedByEma
 		spawnPoint.SetActive(false); 
-
+		//ByEmaStart
+		if (deactivateColliderOnBreak && objectCollider != null)
+		{
+			objectCollider.enabled = false; // Deactivate the collider
+		}
+		StartCoroutine(Respawn());
+		//ByEmaEnd
 	}
 
 
@@ -57,6 +64,24 @@ public class BreakableObject : MonoBehaviour, IBreakable
 			NoHP();
 		}
 	}
+
 	public void Knockback(float _Power)
 	{ }
+	
+	//ByEma: provo un respawn :}
+	private IEnumerator Respawn()
+	{
+		yield return new WaitForSeconds(respawnTime);
+
+		IsBroken = false;
+		BrokenObj.SetActive(false);
+		OriginalObj.SetActive(true);
+		spawnPoint.SetActive(true);
+
+		if (deactivateColliderOnBreak && objectCollider != null)
+		{
+			objectCollider.enabled = true;
+		}
+		HP = 4f;
+	}
 }
