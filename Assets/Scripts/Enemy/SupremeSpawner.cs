feec945af62abdb9eb637c14fd96247266709188
emacrapa <emanuele.crapanzano02@gmail.com>
2024-06-27 04:13:47 +0200

    WeHaveAWincondition

--- Assets/Scripts/Enemy/SupremeSpawner.cs
+++ Assets/Scripts/Enemy/SupremeSpawner.cs
@@ -28,6 +28,5 @@ public class SupremeSpawner : MonoBehaviour
     {
         enemyToSpawn.Transform.position = transform.position;
         enemyToSpawn.Transform.gameObject.SetActive(true);
-        
     }
 }
--- Assets/Scripts/VictoryTrigger.cs
+++ Assets/Scripts/VictoryTrigger.cs
@@ -1,18 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class VictoryTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnDisable()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameManager.OnWin?.Invoke();
     }
 }
